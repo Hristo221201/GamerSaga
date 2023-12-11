e830439285a1df69ff3320ae29a64a1df43de5e2
Hristo <hristo221201@gmail.com>
2023-12-11 20:02:11 +0100

    obtenerAPI

--- src/pages/paginaprincipal.jsx
+++ src/pages/paginaprincipal.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function PaginaPrincipal() {
 
@@ -12,6 +13,43 @@ export default function PaginaPrincipal() {
             document.body.classList.remove('body-paginaprincipal');
         };
     }, [])
+
+
+    const PokemonAleatorio = () => {
+        const [pokemon, setPokemon] = useState(null);
+      
+        useEffect(() => {
+          const obtenerPokemonAleatorio = async () => {
+            // Número total de Pokémon en la API
+            const totalPokemon = 898;  // Este número puede cambiar según la versión de la API
+      
+            // Obtener un número aleatorio entre 1 y el total de Pokémon
+            const numeroPokemon = Math.floor(Math.random() * totalPokemon) + 1;
+      
+            // Construir la URL de la API con el número de Pokémon aleatorio
+            const url = `https://pokeapi.co/api/v2/pokemon/${numeroPokemon}/`;
+      
+            try {
+              // Realizar la solicitud a la API
+              const response = await fetch(url);
+              const data = await response.json();
+      
+              // Actualizar el estado con la información del Pokémon
+              setPokemon({
+                nombre: data.name.capitalize(),
+                id: data.id,
+                tipos: data.types.map(tipo => tipo.type.name.capitalize())
+              });
+            } catch (error) {
+              console.error('Error al obtener información del Pokémon:', error);
+            }
+          };
+      
+          obtenerPokemonAleatorio();
+        }, []); // El segundo argumento [] asegura que useEffect se ejecute solo una vez al montar el componente
+    };
+
+
 
     /* FUNCIONES PAGINA PAGINAPRINCIPAL */
     const goto=useNavigate();
@@ -419,6 +457,16 @@ export default function PaginaPrincipal() {
                             </div>
                             <br/>
                             <p align="center" className="textoEncimaDeSidebar">Aqui tienes 4 pokemons aleatorios</p>
+                            {pokemon ? (
+                                <div>
+                                <h2>Información del Pokémon:</h2>
+                                <p>Nombre: {pokemon.nombre}</p>
+                                <p>ID: {pokemon.id}</p>
+                                <p>Tipos: {pokemon.tipos.join(', ')}</p>
+                                </div>
+                            ) : (
+                                <p>Cargando información del Pokémon...</p>
+                            )};
                         </div>
 
                         <a href="#inicioPagina" className="botonHaciaArriba">
@@ -435,4 +483,4 @@ export default function PaginaPrincipal() {
             </div>
         </>
     );
-};
+}
