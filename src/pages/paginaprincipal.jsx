42caeb44e7530ef902571a0588d12ce7ad476551
Hristo <hristo221201@gmail.com>
2023-12-12 20:49:54 +0100

    introduccionImagenAPIPrueba25

--- src/pages/paginaprincipal.jsx
+++ src/pages/paginaprincipal.jsx
@@ -2,44 +2,6 @@ import { useNavigate } from "react-router-dom";
 import React, { useEffect } from 'react';
 import PokemonAleatorio from './apiPokemon.jsx';
 
-import express from "express";
-import cors from "cors";
-import User from "./bdconection.jsx";
-const app=express();
-app.use(express.json());
-app.use(cors());
-
-app.get("/", async(req, res) => {
-    const snapshot = await User.get();
-    const list = snapshot.docs.map((doc) => ({ id: doc.id,  ...doc.data() }));
-    res.send(list);
-});
-
-app.post("/create", async(req, res) => {
-    const data = req.body;
-    await User.add({ data });
-    res.send({ msg:"User Added" });
-});
-
-app.post("/update", async(req, res) => {
-    const id = req.body.id;
-    delete req.body.id;
-    const data = req.body;
-    await User.doc(id).update(data);
-    res.send({ msg:"Updated" });
-});
-
-app.post("/delete", async(req, res) => {
-    const id = req.body.id;
-    await User.doc(id).delete();
-    res.send({ msg:"Deleted" });
-});
-
-app.listen(4000, () => console.log("Up & Running *4000"));
-
-
-
-
 export default function PaginaPrincipal() {
 
     useEffect(() => {
