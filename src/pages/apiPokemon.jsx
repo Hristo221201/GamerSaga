241678d8f54247d6117d708da6e2a69142b7fb53
Hristo <hristo221201@gmail.com>
2023-12-12 16:40:22 +0100

    introduccionImagenAPIPrueba8

--- src/pages/apiPokemon.jsx
+++ src/pages/apiPokemon.jsx
@@ -48,7 +48,7 @@ export default function PokemonAleatorio() {
     return (
       <div>
         {pokemons.map((pokemon, index) => (
-          <div key={index}>
+          <div>
             <h2>{pokemon.nombre}</h2>
             <img src={pokemon.imagen} alt={pokemon.nombre} />
             <p>Tipos: {pokemon.tipos.join(', ')}</p>
